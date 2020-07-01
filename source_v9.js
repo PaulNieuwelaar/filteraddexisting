c8bf217d901b362b45f107a642939f97df2263fc
Paul Nieuwelaar <14048382+PaulNieuwelaar@users.noreply.github.com>
2020-07-02 09:50:53 +1200

    Updated example for Unified Interface

--- source_v9.js
+++ source_v9.js
@@ -12,7 +12,7 @@ function filterAddExistingContact(selectedEntityTypeName, selectedControl, first
             filters: [{ 
                 entityLogicalName: "contact", 
                 filterXml: "<filter type='and'><condition attribute='parentcustomerid' operator='eq' value='" + Xrm.Page.data.entity.getId() + "' /></filter>" 
-            }];
+            }]
         };
 
         lookupAddExistingRecords("new_account_contact", "account", "contact", firstPrimaryItemId, selectedControl, options);
@@ -28,29 +28,33 @@ function filterAddExistingContact(selectedEntityTypeName, selectedControl, first
 // relatedEntity = the N in the 1:N or the secondary entity in the N:N
 // parentRecordId = the guid of the record this subgrid/related entity is used on
 // gridControl = the grid control parameter passed from the ribbon context
-// lookupOptions = options for creating the custom lookup with filters: http://butenko.pro/2017/11/22/microsoft-dynamics-365-v9-0-lookupobjects-closer-look/
+// lookupOptions = options for creating the custom lookup with filters: https://docs.microsoft.com/en-us/powerapps/developer/model-driven-apps/clientapi/reference/xrm-utility/lookupobjects
 function lookupAddExistingRecords(relationshipName, primaryEntity, relatedEntity, parentRecordId, gridControl, lookupOptions) {
     Xrm.Utility.lookupObjects(lookupOptions).then(function (results) {
-        // Get the entitySet name for the primary entity
-        Xrm.Utility.getEntityMetadata(primaryEntity).then(function (primaryEntityData) {
-            var primaryEntitySetName = primaryEntityData.EntitySetName;
+        if (results.length > 0) {
+            // Get the entitySet name for the primary entity
+            Xrm.Utility.getEntityMetadata(primaryEntity).then(function (primaryEntityData) {
+                var primaryEntitySetName = primaryEntityData.EntitySetName;
 
-            // Get the entitySet name for the related entity
-            Xrm.Utility.getEntityMetadata(relatedEntity).then(function (relatedEntityData) {
-                var relatedEntitySetName = relatedEntityData.EntitySetName;
+                // Get the entitySet name for the related entity
+                Xrm.Utility.getEntityMetadata(relatedEntity).then(function (relatedEntityData) {
+                    var relatedEntitySetName = relatedEntityData.EntitySetName;
 
-                // Call the associate web api for each result (recursive)
-                associateAddExistingResults(relationshipName, primaryEntitySetName, relatedEntitySetName, relatedEntity, parentRecordId.replace("{", "").replace("}", ""), gridControl, results, 0)
+                    // Call the associate web api for each result (recursive)
+                    associateAddExistingResults(relationshipName, primaryEntitySetName, relatedEntitySetName, relatedEntity, parentRecordId.replace("{", "").replace("}", ""), gridControl, results, 0)
+                });
             });
-        });
+        }
     });
 }
 
 // Used internally by the above function
 function associateAddExistingResults(relationshipName, primaryEntitySetName, relatedEntitySetName, relatedEntity, parentRecordId, gridControl, results, index) {
+    var formContext = gridControl.formContext;
+    
     if (index >= results.length) {
         // Refresh the grid once completed
-        Xrm.Page.ui.setFormNotification("Associated " + index + " record" + (index > 1 ? "s" : ""), "INFO", "associate");
+        formContext.ui.setFormNotification("Associated " + index + " record" + (index > 1 ? "s" : ""), "INFO", "associate");
         if (gridControl) { gridControl.refresh(); }
 
         // Clear the final notification after 2 seconds
@@ -61,7 +65,7 @@ function associateAddExistingResults(relationshipName, primaryEntitySetName, rel
         return;
     }
 
-    Xrm.Page.ui.setFormNotification("Associating record " + (index + 1) + " of " + results.length, "INFO", "associate");
+    formContext.ui.setFormNotification("Associating record " + (index + 1) + " of " + results.length, "INFO", "associate");
 
     var lookupId = results[index].id.replace("{", "").replace("}", "");
     var lookupEntity = results[index].entityType || results[index].typename;
@@ -74,10 +78,10 @@ function associateAddExistingResults(relationshipName, primaryEntitySetName, rel
         relatedId = parentRecordId;
     }
 
-    var association = { '@odata.id': Xrm.Page.context.getClientUrl() + "/api/data/v9.0/" + relatedEntitySetName + "(" + relatedId + ")" };
+    var association = { '@odata.id': formContext.context.getClientUrl() + "/api/data/v9.0/" + relatedEntitySetName + "(" + relatedId + ")" };
 
     var req = new XMLHttpRequest();
-    req.open("POST", Xrm.Page.context.getClientUrl() + "/api/data/v9.0/" + primaryEntitySetName + "(" + primaryId + ")/" + relationshipName + "/$ref", true);
+    req.open("POST", formContext.context.getClientUrl() + "/api/data/v9.0/" + primaryEntitySetName + "(" + primaryId + ")/" + relationshipName + "/$ref", true);
     req.setRequestHeader("Accept", "application/json");
     req.setRequestHeader("Content-Type", "application/json; charset=utf-8");
     req.setRequestHeader("OData-MaxVersion", "4.0");
@@ -100,7 +104,7 @@ function associateAddExistingResults(relationshipName, primaryEntitySetName, rel
                 }
                 else {
                     Xrm.Utility.alertDialog(error);
-                    Xrm.Page.ui.clearFormNotification("associate");
+                    formContext.ui.clearFormNotification("associate");
                     if (gridControl) { gridControl.refresh(); }
                 }
             }
